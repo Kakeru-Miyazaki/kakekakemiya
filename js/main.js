8aba5f3ff7f4688057b225cd823a7276b39a5623
kakeru macbook <kakekakemiya920@g.ecc.u-tokyo.ac.jp>
2022-02-08 17:46:42 +0900

    h3:line-height modified

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ $(function () {
       var elemPos = $(this).offset().top;
       var scroll = $(window).scrollTop();
       var windowHeight = $(window).height();
-      if (scroll > elemPos - windowHeight + 250) {
+      if (scroll > elemPos - windowHeight + 100) {
         $(this).addClass('scrollin');
       }
     });
